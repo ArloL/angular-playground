0b37499b604e8a5482b305b6696a9c31965e800c
Arlo O'Keeffe <tiger@k5d.de>
2024-10-31 18:56:14 +0100

    show amount on save button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -19,5 +19,37 @@ export class AppComponent {
   selectedSplit = 0;
   selectSplit(index: number) {
     this.selectedSplit = index;
+  }
+  amount = 0;
+  currency = '€';
+  customParseFloat(str: string) {
+    const commas = (str.match(/,/g) || []).length;
+    if (commas === 1) {
+      str = str.replace(',', '.');
+    } else if (commas > 1) {
+      str = str.replaceAll(',', '');
+    }
+    const dots = (str.match(/\./g) || []).length;
+    if (dots > 1) {
+      str = str.replace('.', '');
+    }
+    return Number.parseFloat(str);
+  }
+
+  formatNumber(number: number, digits = 2) {
+    if (isNaN(number)) {
+      number = 0;
+    }
+    return number.toFixed(digits).toLocaleString();
+  }
+  parseAmountValue(event : Event) {
+    const inputElement = event.target as HTMLInputElement;
+    this.amount = this.customParseFloat(inputElement.value);
+  }
+
+  @ViewChild('inputRef') inputRef!: ElementRef;
+
+  ngAfterViewInit() {
+    this.inputRef.nativeElement.focus();
   }
 }
