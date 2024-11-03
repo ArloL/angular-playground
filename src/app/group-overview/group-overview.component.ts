25d1c91d353f8bb2463f7787548d49070a28d58a
Arlo O'Keeffe <tiger@k5d.de>
2024-11-03 10:37:31 +0100

    add group overview component

---
+++ src/app/group-overview/group-overview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'apezzi-group-overview',
+  standalone: true,
+  imports: [],
+  templateUrl: './group-overview.component.html',
+  styleUrl: './group-overview.component.scss'
+})
+export class GroupOverviewComponent {
+
+}
