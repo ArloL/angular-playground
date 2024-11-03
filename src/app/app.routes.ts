a7ab8517609e5abcf5a2ee3487213145d5466b1a
Arlo O'Keeffe <tiger@k5d.de>
2024-11-03 10:48:08 +0100

    add routing between two components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AddExpenseComponent } from './add-expense/add-expense.component';
+import { GroupOverviewComponent } from './group-overview/group-overview.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'add-expense-component', component: AddExpenseComponent },
+  { path: 'group-overview-component', component: GroupOverviewComponent },
+];
