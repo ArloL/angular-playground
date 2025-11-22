435580196e47442726594272dfcb23bdf99a0b68
Arlo O'Keeffe <tiger@k5d.de>
2025-11-22 21:52:34 +0100

    create and edit groups

--- src/app/groups/group-create/group-create.ts
+++ src/app/groups/group-create/group-create.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { form, Field, debounce, required } from '@angular/forms/signals';
+import { GroupStore } from '../../services/group-store';
+import { generateId } from '../../helper/generate-id';
+import { UserStore } from '../../services/user-store';
+
+interface GroupData { name: string }
 
 @Component({
   selector: 'apezzi-group-create',
-  imports: [],
+  imports: [Field],
   templateUrl: './group-create.html',
   styleUrl: './group-create.scss',
 })
 export class GroupCreate {
+
+  groupStore = inject(GroupStore);
+  userStore = inject(UserStore);
+
+  groupData = signal<GroupData>({ name: '', });
+
+  groupForm = form(this.groupData, (schemaPath) => {
+    debounce(schemaPath.name, 150);
+    required(schemaPath.name)
+  });
+
+  save() {
+    this.groupStore.add({
+      id: generateId(),
+      name: this.groupData().name,
+      users: [],
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      createdBy: this.userStore.first().id,
+    });
+    this.reset();
+  }
+
+  reset() {
+    this.groupForm().reset();
+    this.groupData.set({ name: '', });
+  }
 
 }
--- src/app/groups/group-edit/group-edit.ts
+++ src/app/groups/group-edit/group-edit.ts
@@ -1,14 +1,46 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, effect, inject, input, linkedSignal, signal } from '@angular/core';
 import { GroupId } from '../../models/group';
+import { GroupStore } from '../../services/group-store';
+import { debounce, Field, form, required } from '@angular/forms/signals';
+import { Router } from '@angular/router';
+
+interface GroupData { name: string }
 
 @Component({
   selector: 'apezzi-group-edit',
-  imports: [],
+  imports: [Field],
   templateUrl: './group-edit.html',
   styleUrl: './group-edit.scss',
 })
 export class GroupEdit {
 
   readonly groupId = input.required<GroupId>();
+
+  private groupStore = inject(GroupStore);
+  private router = inject(Router);
+
+  groupData = linkedSignal<GroupData>(() => {
+    var group = this.groupStore.findById(this.groupId());
+    if (group) {
+      return { name: group.name };
+    } else {
+      this.router.navigate(['/']);
+      return { name: '' };
+    }
+  });
+
+  groupForm = form(this.groupData, (schemaPath) => {
+    debounce(schemaPath.name, 150);
+    required(schemaPath.name)
+  });
+
+  save() {
+    this.groupStore.findById(this.groupId()).name = this.groupData().name;
+    this.reset();
+  }
+
+  reset() {
+    this.groupForm().reset();
+  }
 
 }
