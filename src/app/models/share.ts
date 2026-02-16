00adbcb50160759a5132aa850a68eb2ce401b5bf
Arlo O'Keeffe <tiger@k5d.de>
2026-02-16 15:15:48 +0100

    extract

--- src/app/models/share.ts
+++ src/app/models/share.ts
@@ -1,5 +1,11 @@
 import { EntityId } from "./entity";
 
+export interface ShareRaw {
+  userId: EntityId,
+  owed: string,
+  included: boolean,
+}
+
 export interface Share {
   userId: EntityId,
   owed: number,
