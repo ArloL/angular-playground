1194b3a52d5db8faad5d74b2644ef640f9b32051
Arlo O'Keeffe <tiger@k5d.de>
2026-02-17 11:27:00 +0100

    group members

--- src/app/groups/group-view/group-view.ts
+++ src/app/groups/group-view/group-view.ts
@@ -1,8 +1,8 @@
 import { Component, inject, input, resource } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { Entity, EntityId } from '../../models/entity';
+import { EntityId } from '../../models/entity';
 import { GroupStore } from '../../services/group-store';
-import { Group } from '../../models/group';
+import { UserStore } from '../../services/user-store';
 
 @Component({
   selector: 'apezzi-group-view',
@@ -15,10 +15,15 @@ export class GroupView {
   readonly groupId = input.required<EntityId>();
 
   private groupStore = inject(GroupStore);
+  private userStore = inject(UserStore);
 
-  group = resource({
+  resourceData = resource({
     params: () => ({ id: this.groupId() }),
-    loader: ({ params }) => this.groupStore.findById(params.id),
+    loader: async ({ params }) => {
+      const group = await this.groupStore.findById(params.id);
+      const members = await this.userStore.findByIds(group.users);
+      return { group, members };
+    },
   });
 
 }
