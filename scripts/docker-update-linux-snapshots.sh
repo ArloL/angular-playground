405dec0511dcbd358059b0c8b48e82fbe8a3b0de
Arlo O'Keeffe <tiger@k5d.de>
2026-02-24 22:18:26 +0100

    renovate

--- scripts/docker-update-linux-snapshots.sh
+++ scripts/docker-update-linux-snapshots.sh
@@ -3,9 +3,12 @@ set -o errexit
 set -o nounset
 set -o xtrace
 
+# renovate: datasource=docker depName=mcr.microsoft.com/playwright
+PLAYWRIGHT_IMAGE="mcr.microsoft.com/playwright:v1.56.1-noble"
+
 docker run --rm \
   --mount "type=bind,src=$(pwd),dst=/work" \
   --mount "type=volume,dst=/work/node_modules" \
   --workdir /work \
-  mcr.microsoft.com/playwright:v1.56.1-noble \
+  "$PLAYWRIGHT_IMAGE" \
   bash -c "npm ci && npm run e2e:update"
