089348f8042480968310da90ec7b3c8bb6ace03e
Arlo O'Keeffe <tiger@k5d.de>
2026-02-24 21:56:15 +0100

    protect node_modules

--- scripts/docker-update-linux-snapshots.sh
+++ scripts/docker-update-linux-snapshots.sh
@@ -4,7 +4,8 @@ set -o nounset
 set -o xtrace
 
 docker run --rm \
-  -v "$(pwd):/work" \
-  -w /work \
+  --mount "type=bind,src=$(pwd),dst=/work" \
+  --mount "type=volume,dst=/work/node_modules" \
+  --workdir /work \
   mcr.microsoft.com/playwright:v1.56.1-noble \
   bash -c "npm ci && npm run e2e:update"
