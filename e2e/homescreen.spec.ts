a5a7eef21853bc8193671ba00f6bf83f10da879d
Arlo O'Keeffe <tiger@k5d.de>
2026-02-24 21:31:32 +0100

    e2e

--- e2e/homescreen.spec.ts
+++ e2e/homescreen.spec.ts
@@ -3,6 +3,8 @@ import { expect, test } from '@playwright/test';
 test('homescreen matches visual baseline', async ({ page }) => {
   await page.goto('/');
   await page.waitForLoadState('networkidle');
+  await page.getByTestId("button-login").click();
+  await page.getByTestId("link-group-personal").waitFor();
 
   await expect(page).toHaveScreenshot('homescreen.png', {
     maxDiffPixelRatio: 0.01,
